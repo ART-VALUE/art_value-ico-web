888bf560044b7b6325bc03aaba1dcf88b1eef579
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-04-07 10:43:36 +0200

    added links to Header and styled them

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,82 +1,73 @@
-import './App.scss'
-import FlipClock from './Flipclock'
-import { loadStripe } from "@stripe/stripe-js"
-import Modal from 'react-modal'
-import PurchaseDialog from './purchaseDialog/PurchaseDialog'
-import { useState } from 'react'
-import { io } from 'socket.io-client'
-import { PAYMENT_NS } from '../api'
-import { 
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom'
-import PreIco from './PreIco'
-import { ThemeProvider } from 'styled-components'
-import { darken, lighten } from "polished";
-import Ico from './Ico'
+import './App.scss';
+import FlipClock from './Flipclock';
+import { loadStripe } from '@stripe/stripe-js';
+import Modal from 'react-modal';
+import PurchaseDialog from './purchaseDialog/PurchaseDialog';
+import Header from './Header';
+import { useState } from 'react';
+import { io } from 'socket.io-client';
+import { PAYMENT_NS } from '../api';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PreIco from './PreIco';
+import { ThemeProvider } from 'styled-components';
+import { darken, lighten } from 'polished';
+import Ico from './Ico';
 
-const stripePromise = loadStripe("pk_test_51ITna8JcDKL67QxNOgLJSMRVqFGagqi7pcHXCtewhqPyxqsWt5QrIWLeCoWXTZlZIi2gOhNSh3JE4M6Rr40VFhSJ00cmqxUD2d");
-const paymentApiIo = io(`http://localhost:3000/${PAYMENT_NS}`)
+const stripePromise = loadStripe(
+	'pk_test_51ITna8JcDKL67QxNOgLJSMRVqFGagqi7pcHXCtewhqPyxqsWt5QrIWLeCoWXTZlZIi2gOhNSh3JE4M6Rr40VFhSJ00cmqxUD2d'
+);
+const paymentApiIo = io(`http://localhost:3000/${PAYMENT_NS}`);
 
-Modal.setAppElement("#root")
+Modal.setAppElement('#root');
 
-const colorBackground = "#0a0a0a"
+const colorBackground = '#0a0a0a';
 
 const theme = {
-  color: {
-    background: colorBackground,
-    surface: lighten(0.2, colorBackground),
-    border: lighten(0.4, colorBackground),
-    text: darken(0.1, "#fff")
-  },
-  font: {
-    text: "Avenir95Light",
-    title: "Avenir95Black"
-  }
-}
+	color: {
+		background: colorBackground,
+		surface: lighten(0.2, colorBackground),
+		border: lighten(0.4, colorBackground),
+		text: darken(0.1, '#fff'),
+	},
+	font: {
+		text: 'Avenir95Light',
+		title: 'Avenir95Black',
+	},
+};
 
 export default function App() {
-  const [purchaseDialogOpen, setPurchaseDialogOpen] = useState(false)
-  const endDate = new Date(1615809936000).getMilliseconds()
+	const [purchaseDialogOpen, setPurchaseDialogOpen] = useState(false);
+	const endDate = new Date(1615809936000).getMilliseconds();
 
-  const handlePurchaseTokenClick = () => {
-    setPurchaseDialogOpen(true)
-  }
+	const handlePurchaseTokenClick = () => {
+		setPurchaseDialogOpen(true);
+	};
 
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Modal
-            isOpen={purchaseDialogOpen}
-            className="PurchaseDialogModal"
-            overlayClassName="PurchaseDialogModalOverlay"
-            contentLabel="Coin Purchase Dialog"
-            shouldCloseOnOverlayClick={true}>
-            <PurchaseDialog
-              stripePromise={stripePromise}
-              paymentApiIo={paymentApiIo} />
-          </Modal>
+	return (
+		<div className="App">
+			<ThemeProvider theme={theme}>
+				<Router>
+					<Modal
+						isOpen={purchaseDialogOpen}
+						className="PurchaseDialogModal"
+						overlayClassName="PurchaseDialogModalOverlay"
+						contentLabel="Coin Purchase Dialog"
+						shouldCloseOnOverlayClick={true}
+					>
+						<PurchaseDialog stripePromise={stripePromise} paymentApiIo={paymentApiIo} />
+					</Modal>
+					<Header />
+					<Switch>
+						<Route path="/">
+							<Ico />
+						</Route>
+					</Switch>
 
-          <header className="App-header">
-            {/* <Link to="/"></Link> */}
-            <a href="#">Linkielink</a>
-            <a href="#">Linkielink2</a>
-          </header>
-
-          <Switch>
-            <Route path="/">
-              <Ico />
-            </Route>
-          </Switch>
-
-          {/* <div className="ClockCont">
+					{/* <div className="ClockCont">
             <button className="purchase-token" onClick={handlePurchaseTokenClick}>Purchase Art_Value token</button>
           </div> */}
-        </Router>
-      </ThemeProvider>
-    </div>
-  )
+				</Router>
+			</ThemeProvider>
+		</div>
+	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,54 @@
-import { FunctionComponent } from "react";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { baseText, baseTitle } from './styledComponents';
+import logo from '../assets/images/art-value-logo.webp';
 
-const Header: FunctionComponent<{}> = () => {
-    return (
-        <div>Header</div>
-    )
-}
+const HeaderWrapper = styled.div`
+	${baseTitle}
+	border-bottom: 2px solid ${(props) => props.theme.color.border};
+	background-color: ${(props) => props.theme.color.surface};
+	height: 10vh;
+	text-decoration: none;
+`;
 
-export default Header
+const className = 'nav-item';
+const activeClassName = 'nav-item-active';
+
+const StyledLink = styled(NavLink).attrs({ className, activeClassName })`
+	${baseText}
+	&.${className} {
+		text-decoration: none;
+	}
+	&.${activeClassName} {
+		font-weight: bold;
+	}
+`;
+
+const OngoingAuction = 'Ongoing Auction';
+const Gallery = 'Gallery';
+const Calender = 'Calender';
+const MyProfile = 'My Profile';
+
+const Header = () => {
+	return (
+		<HeaderWrapper>
+			<StyledLink to="/" exact>
+				<img alt="art value logo" src={logo} width="100px" />
+			</StyledLink>
+			<StyledLink to="/ongoing-auction" exact>
+				{OngoingAuction}
+			</StyledLink>
+			<StyledLink to="/gallery" exact>
+				{Gallery}
+			</StyledLink>
+			<StyledLink to="/calender" exact>
+				{Calender}
+			</StyledLink>
+			<StyledLink to="/my-profile" exact>
+				{MyProfile}
+			</StyledLink>
+		</HeaderWrapper>
+	);
+};
+
+export default Header;
