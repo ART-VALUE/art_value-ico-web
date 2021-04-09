a74fe700c67ff289514c700d264c03b7c95e7a6a
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-04-09 16:19:23 +0200

    Moved desktop view to separate component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,116 +1,20 @@
-import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import { baseText, baseTitle } from '../styledComponents';
+import { baseTitle } from '../styledComponents';
 import { HeaderDesktop } from './HeaderDesktop';
 import { HeaderMobile } from './HeaderMobile';
 import artValueLogo from '../../assets/images/art-value-logo.webp';
-import RoutingPaths from '../../routes/RoutingPaths';
 
 const HeaderWrapper = styled.div`
 	${baseTitle}
 `;
 
-const Grid = styled.div`
-	display: grid;
-	grid-gap: 1rem;
-	justify-items: center;
-	padding: 1em;
-	@media (min-width: 768px) {
-		grid-template-columns:
-			min-content
-			min-content
-			auto;
-		grid-template-areas: 'logo nav user';
-		align-items: center;
-		padding: 0;
-		border-bottom: 2px solid ${(props) => props.theme.color.border};
-	}
-`;
-
-const Logo = styled.img`
-	width: auto;
-	height: 60px;
-	@media (min-width: 768px) {
-		grid-area: logo;
-		justify-self: start;
-		margin: 0;
-		padding: 0;
-	}
-`;
-
-const NavList = styled.ul`
-	@media (min-width: 768px) {
-		grid-area: nav;
-		place-self: center;
-		display: grid;
-		grid-auto-flow: column;
-		grid-auto-columns: max-content;
-		gap: 0.5rem;
-		list-style: none;
-	}
-`;
-
-const NavListItem = styled.li`
-	@media (min-width: 768px) {
-		margin-right: 0.5em;
-	}
-`;
-
-const User = styled.div`
-	@media (min-width: 768px) {
-		grid-area: user;
-		justify-self: end;
-		margin-right: 0.5em;
-	}
-`;
-
-/* Add boolean to control bold or not */
-const StyledLink = styled.span`
-	${baseText}
-	font-weight: bold;
-	text-decoration: none;
-	&:hover {
-		opacity: 0.6;
-	}
-	@media (max-width: 768px) {
-		font-size: 1.2em;
-		min-height: 50px;
-		&:hover {
-			opacity: 1;
-		}
-	}
-`;
-
-const OngoingAuction: string = 'Ongoing Auction';
-const Gallery: string = 'Gallery';
-const Calender: string = 'Calender';
-const MyProfile: string = 'My Profile';
+let HeaderTabs: string[] = [];
+HeaderTabs = ['Ongoing Auction', 'Gallery', 'Calender', 'My Profile'];
 
 const Header = () => {
-	const history = useHistory();
 	return (
 		<HeaderWrapper>
-			<Grid>
-				<Logo
-					alt="art value logo"
-					src={artValueLogo}
-					onClick={() => history.push(RoutingPaths.homeView)}
-				></Logo>
-				<NavList>
-					<NavListItem>
-						<StyledLink onClick={() => history.push(RoutingPaths.auctionView)}>{OngoingAuction}</StyledLink>
-					</NavListItem>
-					<NavListItem>
-						<StyledLink onClick={() => history.push(RoutingPaths.galleryView)}>{Gallery}</StyledLink>
-					</NavListItem>
-					<NavListItem>
-						<StyledLink onClick={() => history.push(RoutingPaths.calenderView)}>{Calender}</StyledLink>
-					</NavListItem>
-				</NavList>
-				<User>
-					<StyledLink onClick={() => history.push(RoutingPaths.profileView)}>{MyProfile}</StyledLink>
-				</User>
-			</Grid>
+			<HeaderDesktop logo={artValueLogo} tabs={HeaderTabs} />
 		</HeaderWrapper>
 	);
 };
--- src/components/Header/HeaderDesktop.tsx
+++ src/components/Header/HeaderDesktop.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import { FunctionComponent } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { baseText } from '../styledComponents';
+import RoutingPaths from '../../routes/RoutingPaths';
 
-export const HeaderDesktop = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Grid = styled.div`
+	display: grid;
+	grid-gap: 1rem;
+	justify-items: center;
+	padding: 1em;
+	grid-template-columns:
+		min-content
+		min-content
+		auto;
+	grid-template-areas: 'logo nav user';
+	align-items: center;
+	padding: 0;
+	border-bottom: 2px solid ${(props) => props.theme.color.border};
+`;
+
+const className: string = 'x';
+const StyledLink = styled.span.attrs({ className })`
+	${baseText}
+	text-decoration: none;
+	&. ${className} {
+		font-weight: bold;
+	}
+	&:hover {
+		opacity: 0.6;
+	}
+`;
+
+const Logo = styled.img`
+	width: auto;
+	height: 60px;
+	grid-area: logo;
+	justify-self: start;
+	margin: 0;
+	padding: 0;
+	cursor: pointer;
+`;
+
+const NavList = styled.ul`
+	grid-area: nav;
+	place-self: center;
+	display: grid;
+	grid-auto-flow: column;
+	grid-auto-columns: max-content;
+	gap: 0.5rem;
+	list-style: none;
+	cursor: pointer;
+`;
+
+const NavListItem = styled.li`
+	margin-right: 0.5em;
+`;
+
+const User = styled.div`
+	grid-area: user;
+	justify-self: end;
+	margin-right: 0.5em;
+	cursor: pointer;
+`;
+
+type HeaderDesktopLogo = {
+	logo: string;
+	tabs: Array<string>;
+};
+
+export const HeaderDesktop: FunctionComponent<HeaderDesktopLogo> = ({ logo, tabs }) => {
+	const history = useHistory();
+	const currentRoute = useHistory().location.pathname.toLowerCase();
+	return (
+		<Grid>
+			<Logo alt="art value logo" src={logo} onClick={() => history.push(RoutingPaths.homeView)}></Logo>
+			<NavList>
+				<NavListItem>
+					<StyledLink
+						className={currentRoute.includes(RoutingPaths.auctionView) ? 'active' : ''}
+						onClick={() => history.push(RoutingPaths.auctionView)}
+					>
+						{tabs[0]}
+					</StyledLink>
+				</NavListItem>
+				<NavListItem>
+					<StyledLink onClick={() => history.push(RoutingPaths.galleryView)}>{tabs[1]}</StyledLink>
+				</NavListItem>
+				<NavListItem>
+					<StyledLink onClick={() => history.push(RoutingPaths.calenderView)}>{tabs[2]}</StyledLink>
+				</NavListItem>
+			</NavList>
+			<User>
+				<StyledLink onClick={() => history.push(RoutingPaths.profileView)}>{tabs[3]}</StyledLink>
+			</User>
+		</Grid>
+	);
+};
